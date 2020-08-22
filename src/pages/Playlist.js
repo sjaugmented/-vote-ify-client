9902706755319bf4521c7265fb4455306d1b797d
Evan Doherty <edoherty77@gmail.com>
2020-08-22 00:09:50 -0400

    used ant to style layouts

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Sidebar from '../components/Sidebar'
+import SongList from '../components/SongList'
+import HeadContainer from '../components/HeadContainer'
+
+
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class Playlist extends Component {
   render() {
     return (
-      <div>
-        <h1>Playlist Page</h1>
-      </div>
+      <Layout>
+        {/* <Header><HeadContainer/></Header>
+        <Layout> */}
+          <Content><SongList /></Content>
+          <Sider><Sidebar /></Sider>
+        {/* </Layout> */}
+      </Layout>
     );
   }
 }
