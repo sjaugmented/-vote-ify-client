ef11374c34753e9855c1b5ca5a04005f89d75105
Evan Doherty <edoherty77@gmail.com>
2020-08-21 20:21:48 -0400

    installed react-router-dom. added our 3 pages

---
+++ src/pages/Playlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Playlist Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
