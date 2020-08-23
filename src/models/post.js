56bd0f39f182489f375ccb46fee521fade32d816
sjaugmented <sjaugmented@gmail.com>
2020-08-23 09:46:46 -0700

    potential create route in PostModel

--- src/models/post.js
+++ src/models/post.js
@@ -1,9 +1,17 @@
+const axios = require('axios')
 const url = 'http://localhost:3001/api/v1'
 
 export default class PostModel {
     static create = async (post) => {
         try {
-            
+            const newPost = await axios.get(`${url}/posts`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(post)
+            })
+            console.log(newPost)
         } catch (error) {
             console.log(error)
         }
