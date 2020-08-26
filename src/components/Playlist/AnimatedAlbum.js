cec90e842ad02bb043200d2fc6c591264b249b6d
sjaugmented <sjaugmented@gmail.com>
2020-08-26 16:17:33 -0700

    started transferring animated album styling to SongList

--- src/components/Playlist/AnimatedAlbum.js
+++ src/components/Playlist/AnimatedAlbum.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { useSpring, animated } from 'react-spring'
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 function AnimatedAlbum(playlist) {
-console.log(playlist.playlist.coverart)
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
   return (
     <animated.div
