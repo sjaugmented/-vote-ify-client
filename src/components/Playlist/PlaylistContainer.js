da529db0460f39574b1da2bfc0d4d302136d5a3b
Evan Doherty <edoherty77@gmail.com>
2020-08-25 21:01:16 -0400

    cleaned up a buncha shit

--- src/components/Playlist/PlaylistContainer.js
+++ src/components/Playlist/PlaylistContainer.js
@@ -1,22 +1,40 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
+//import components
+import Sidebar from './Sidebar'
+import SongList from './SongList';
+
+//import styles
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
-
-
+import { LeftCircleTwoTone, RightCircleTwoTone } from '@ant-design/icons';
 const { Header, Footer, Sider, Content } = Layout;
-const PlaylistContainer = ({playlist, spotPlaylist}, props) => {
-  console.log({spotPlaylist})
-  return (
-    <div className='playContainerDiv'>
-      <header className='playlistHeader'>
-        {playlist && playlist.playlist.coverart ? <img src={playlist.playlist.coverart} /> : 'loading...'}
-        <h1>{playlist && playlist.playlist.title ? playlist.playlist.title : 'loading...'}</h1>
-        <button onClick={props.toggle}>Toggle</button>
-      </header>
-        
 
-    </div>
+
+const PlaylistContainer = ({playlist}) => {
+   //Toggle sidebar functionality
+   const [isHidden, setIsHidden] = useState(false)
+   const toggle =() => {
+     setIsHidden(!isHidden)
+   }
+ 
+  return (
+    <Layout>
+      <Content>
+        <header className='playlistHeader'>
+          {playlist && playlist.playlist.coverart ? <img src={playlist.playlist.coverart} /> : 'loading...'}
+          <h1>{playlist && playlist.playlist.title ? playlist.playlist.title : 'loading...'}</h1>
+          <button onClick={toggle}>{isHidden ? <LeftCircleTwoTone /> : <RightCircleTwoTone />}</button>
+        </header>
+
+        <section>
+          <SongList playlist={playlist} />
+        </section>
+      </Content>
+      <Sider id='sider' className={isHidden ? 'hide' : 'show'}>
+        <Sidebar />
+      </Sider>
+    </Layout>
   )
   
 }
--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -15,12 +15,7 @@ import 'antd/dist/antd.css';
 const { Sider, Content } = Layout;
 
 const Playlist = (props) => {
-  //Toggle sidebar functionality
-  const [isHidden, setIsHidden] = useState(false)
-  const toggle =() => {
-    setIsHidden(!isHidden)
-  }
-
+ 
   //call fetch request to show the single playlist
   const [playlist, setPlaylist] = useState()
   
@@ -29,13 +24,8 @@ const Playlist = (props) => {
     setPlaylist({playlist: result.playlist})
   }
 
+  //spotify api get request - test
   const [spotPlaylist, setSpotPlaylist] = useState()
-  
-  useEffect(() => {
-    getPlaylist()
-    spotifyPlaylist()
-  }, []);
-
   const spotifyPlaylist = async () => {
     console.log(props.token)
     const showPlaylist = await SpotifyModel.playlist(props.token)
@@ -43,19 +33,13 @@ const Playlist = (props) => {
     setSpotPlaylist({spotPlaylist: showPlaylist})
   }
 
+  useEffect(() => {
+    getPlaylist()
+    spotifyPlaylist()
+  }, []);
+
   return (
-    <Layout>
-      {/* Header is here */}
-      <Content>
-        <PlaylistContainer toggle={toggle} playlist={playlist} spotPlaylist={spotPlaylist}/>
-        <SongList playlist={playlist} />
-        <button onClick={spotifyPlaylist}>Test Endpoint</button>
-      </Content>
-      <Sider id='sider' className={isHidden ? 'hide' : 'show'}>
-        <Sidebar />
-      </Sider>
-      {/* Footer is here */}
-    </Layout>
+    <PlaylistContainer playlist={playlist}/>
   );
       
     
