451995a42487c0b299b43bd60678ce03e17836d0
sjaugmented <sjaugmented@gmail.com>
2020-08-27 21:07:18 -0700

    reset cards and styling

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -9,8 +9,9 @@ const Card = (props) => {
   const [flipped, set] = useState(false)
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 }
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg) `,
+    config: { mass: 5, tension: 500, friction: 80 },
+    
   })
 
   return (
