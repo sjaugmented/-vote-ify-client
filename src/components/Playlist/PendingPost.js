2aa4a5130bf8242823b985bd2622bd5e403d7fcb
sjaugmented <sjaugmented@gmail.com>
2020-08-28 18:08:58 -0700

    created PendingPost comp; got upvote toggle working and displaying votes from state

---
+++ src/components/Playlist/PendingPost.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import 'antd/dist/antd.css';
+import { DislikeOutlined, LikeTwoTone } from '@ant-design/icons';
+
+const PendingPost = ({ post, key }) => {
+    const [upVote, setUpVote] = useState(true)
+    const [votes, setVotes] = useState(post.votes)
+  
+  
+    console.log(post.votes)
+
+    const handleVote = (post) => {
+      upVote ? setVotes(votes + 1) : setVotes(votes - 1)
+      setUpVote(!upVote)
+      console.log(votes)
+    }
+    
+    return (
+        <div>
+            <li>{post.artist} - {post.songName}</li>
+              <li><button onClick={() => {handleVote(post)}}>
+                {upVote ? <LikeTwoTone /> : <DislikeOutlined />}
+              </button></li>
+            <li><p>{votes}</p></li>
+        </div>
+    )
+}
+
+export default PendingPost
--- src/components/Playlist/Sidebar.js
+++ src/components/Playlist/Sidebar.js
@@ -1,44 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import 'antd/dist/antd.css';
 import { DislikeOutlined, LikeTwoTone } from '@ant-design/icons';
-
+import PendingPost from './PendingPost'
 
 
 const Sidebar = ({playlist, isPending}) => {
-  const [increment, setIncrement] = useState(true)
   
-  console.log(isPending)
   let posts
   
   if (playlist) {
-    posts = playlist.playlist.posts
+    posts = playlist.playlist.posts.map((post, index) => {
+          return (
+            <PendingPost 
+              key={index}
+              post={post}
+            />
+          )
+        })
   }
-  let songVotes
-  // const [isPending, setPending] = useState(true)
-  const [votes, setVote] = useState(songVotes)
- 
-  const handleVote = (post) => {
-    increment ? setVote(post.vote++) : setVote(post.vote--)
-    setIncrement(!increment)
-  }
+
 
 
   return (
     <div className='sidebarDiv'>
       <h1>Pending Songs</h1>
       <ul>
-        {posts ? posts.map((post, index)=>{
-          songVotes = post.votes
-          return (
-            <>
-            <li>{post.artist} - {post.songName}</li>
-              <li><button onClick={() => {setVote(post.votes++)}}>
-                {increment ? <LikeTwoTone /> : <DislikeOutlined />}
-              </button></li>
-            <li><p>{post.votes}</p></li>
-            </>
-          )
-        }) : 'loading...'}
+        {posts ? posts : 'loading...'}
       </ul>
       
     
