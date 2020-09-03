a0bbef34d9516142d925faed3976615d65782399
sjaugmented <sjaugmented@gmail.com>
2020-09-03 16:04:28 -0700

    BACK TO LOCALHOST

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App(props) {
 
   const fetchLogin = async () => {
     try {
-      const result = await fetch(apiUrl, {
+      const result = await fetch('http://localhost:3001/api/v1/auth/verify', {
         credentials: 'include'
       })
       const data = await result.json()
