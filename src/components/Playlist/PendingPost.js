c5b10633a8c061947390fbb9d776707ff42e39b8
sjaugmented <sjaugmented@gmail.com>
2020-09-04 15:52:05 -0700

    voting seems to be working correctly

--- src/components/Playlist/PendingPost.js
+++ src/components/Playlist/PendingPost.js
@@ -4,27 +4,28 @@ import { DislikeOutlined, LikeTwoTone } from '@ant-design/icons';
 import usePersistedState from '../../hooks/usePersistedState'
 
 const PendingPost = ({ post, key, updateVotes, updatePlayer }) => {
-  const [upVote, setUpVote] = usePersistedState(post.id + 'upvote', 'false')
+  const [downVote, setUpVote] = usePersistedState(`${post._id} downvote`, 'false')
   const [votes, setVotes] = useState(post.votes)
 
   const handleVote = (post) => {
     let newVotes
-    if (upVote) {
-      newVotes = votes + 1
-    } else {
+    if (downVote) {
       newVotes = votes - 1
+    } else {
+      newVotes = votes + 1
     }
     setVotes(newVotes)
-    setUpVote(!upVote)
+    setUpVote(!downVote)
     updateVotes(post, newVotes)
   }
   
   return (
     <div className='pending-post'>
       <li className='pendingSong' onClick={() => { updatePlayer(post.songId) }}>{post.artist} - {post.songName}</li>
+      
       <li >
         <button className='voteBtn' onClick={() => { handleVote(post) }}>
-              {upVote ? <LikeTwoTone /> : <DislikeOutlined />}
+              {downVote ? <DislikeOutlined /> : <LikeTwoTone /> }
         </button>
         <p className='voteCount'>{votes}</p>
         <p className='contributor'> - Suggested by: {post.user}</p>
--- src/hooks/usePersistedState.js
+++ src/hooks/usePersistedState.js
@@ -1,15 +1,14 @@
 import { useState, useEffect } from 'react'
 
 function usePersistedState(key, defaultValue) {
-    // where the magic happens
     const [state, setState] = useState(() =>
-        JSON.parse(localStorage.getItem(key)) || defaultValue
+        JSON.parse(localStorage.getItem(key) || defaultValue)
     )
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(state))
     }, [key, state])
     
-    return [state,setState]
+    return [state, setState]
 }
 
 export default usePersistedState
