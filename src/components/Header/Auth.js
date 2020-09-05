129a797fceba9bb3c517be91fa6c662760ae6b3c
sjaugmented <sjaugmented@gmail.com>
2020-09-04 18:25:15 -0700

    refactored for easy local/heroku switching

--- src/components/Header/Auth.js
+++ src/components/Header/Auth.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+// API's
+const local = 'https://localhost:3000/api/v1'
+const heroku = 'https://spotify-us-api.herokuapp.com/api/v1'
 
 const Auth = (props) => {
-
-  
 
   return (
     <div className='authDiv'>
       {props.username ? 
         <>
           <Link to=''>Hey {props.username}!</Link>
-          <a href={'https://spotify-us-api.herokuapp.com/api/v1/auth/login'}>Refresh Spotify</a>
+          <a href={`${local}/auth/login`}>Refresh Spotify</a>
         </>
         :
-        <a href="https://spotify-us-api.herokuapp.com/api/v1/auth/login"> Connect with Spotify </a>
+        <a href={`${local}/auth/login`}> Connect with Spotify </a>
       }
     </div>
   );
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
-const apiUrl = 'https://spotify-us-api.herokuapp.com/api/v1'
+// API's
+const local = 'https://localhost:3000/api/v1'
+const heroku = 'https://spotify-us-api.herokuapp.com/api/v1'
 
 export default class UserModel {
     
     static show = async (spotifyId) => {
-        const user = await axios.get(`${apiUrl}/users/${spotifyId}`)
+        const user = await axios.get(`${local}/users/${spotifyId}`)
         return user.data
     }
 }
