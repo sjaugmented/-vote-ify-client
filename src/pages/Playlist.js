9dd4679f583bf132c1eee0c78a7c5837f9bf222d
sjaugmented <sjaugmented@gmail.com>
2020-09-06 10:48:28 -0700

    tutorial popup and styling

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -13,7 +13,9 @@ import '../components/Playlist/songList.css'
 import 'antd/dist/antd.css';
 
 const Playlist = (props) => {
-  const [visited, setVisited] = useState(false)
+  const [visited, setVisited] = useState(localStorage.getItem('visited'))
+
+  console.log(visited)
 
   //call fetch request to show the single playlist
   const [playlist, setPlaylist] = useState()
@@ -32,16 +34,21 @@ const Playlist = (props) => {
     localStorage.setItem('visited', true)
   }
 
-  
+  // if (localStorage.getItem('visited')) setVisited(true)
 
   return (
     <>
       { !visited ?
-        <Popup open={visited} closeOnDocumentClick onClose={markVisited}>
-          <div className="modal">
-            <p>This is the Playlist page. You can use the 'Suggest a song' search bar at the top to find songs on Spotify and suggest them to be added to the playlist.</p>
-            <p>Once a song is suggested, it will show up in the sidebar on the right where other members of the community can like them.</p>
-            <p>Once a song reaches the required number of likes, it will be added to the Playlist proper.</p>
+        <Popup open={!visited} closeOnDocumentClick onClose={markVisited}>
+          <div className="modal tutorial">
+            <p><span className='vote-ify'>Welcome to Vote-ify!</span></p>
+            <p>Here, we're all about playlists that are curated by you, the community.</p>
+            <p>To start, use the 'Suggest a song' search bar at the top of each playlist to find songs on Spotify and suggest them.</p>
+            <p>Suggested songs will show up in the 'Vote Songs In!' sidebar on the right where other members of the community can like them.</p>
+            <p>Once a song reaches the required number of hearts, it will officially be added to the playlist proper.</p>
+            <p> To listen to tracks, just click them and they will be loaded into the player at the bottom of the screen. <span className='highlight'> Sorry, we don 't have autoplay working just yet</span>, so you'
+            ll have to manually hit play. </p>
+            <button onClick={markVisited}>Thanks. Got it.</button>
           </div>
         </Popup>
         :
