3194e298bb3ca3171916223efdfc0dc1cf1678ae
sjaugmented <sjaugmented@gmail.com>
2020-09-06 10:05:43 -0700

    fixed playlist table max-height; started tutorial popup

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 //import model
 import PlaylistModel from '../models/playlist'
+import Popup from 'reactjs-popup'
 
 //import components
 import PlaylistContainer from '../components/Playlist/PlaylistContainer';
@@ -12,6 +13,7 @@ import '../components/Playlist/songList.css'
 import 'antd/dist/antd.css';
 
 const Playlist = (props) => {
+  const [visited, setVisited] = useState(false)
 
   //call fetch request to show the single playlist
   const [playlist, setPlaylist] = useState()
@@ -25,9 +27,28 @@ const Playlist = (props) => {
     getPlaylist()
   }, []);
 
-  return (
+  const markVisited = () => {
+    setVisited(true)
+    localStorage.setItem('visited', true)
+  }
 
-    <PlaylistContainer updatePlayer={props.updatePlayer} match={props.match} username={props.username} spotifyId={props.spotifyId} admin={props.admin} accessToken={props.accessToken} playlist={playlist} getPlaylist={getPlaylist}/>
+  
+
+  return (
+    <>
+      { !visited ?
+        <Popup open={visited} closeOnDocumentClick onClose={markVisited}>
+          <div className="modal">
+            <p>This is the Playlist page. You can use the 'Suggest a song' search bar at the top to find songs on Spotify and suggest them to be added to the playlist.</p>
+            <p>Once a song is suggested, it will show up in the sidebar on the right where other members of the community can like them.</p>
+            <p>Once a song reaches the required number of likes, it will be added to the Playlist proper.</p>
+          </div>
+        </Popup>
+        :
+        ''
+      }
+      <PlaylistContainer updatePlayer={props.updatePlayer} match={props.match} username={props.username} spotifyId={props.spotifyId} admin={props.admin} accessToken={props.accessToken} playlist={playlist} getPlaylist={getPlaylist} />
+    </>
   );   
 
 }
